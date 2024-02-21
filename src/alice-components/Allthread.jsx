41f07ed3590329675a3f4957214d0007323b9e2f
alicetra <alicetra0109@gmail.com>
2024-02-21 16:18:09 +1100

    fixed the fact that the toggle is at the top of the page now and not above every single post

--- src/alice-components/Allthread.jsx
+++ src/alice-components/Allthread.jsx
@@ -16,6 +16,7 @@ async function fetchPostData() {
 function Allthread() {
   const [posts, setPosts] = useState([])
   const [sortOrder, setSortOrder] = useState('descending')
+  const [likes, setLikes] = useState()
 
   useEffect(() => {
     fetchPostData().then((data) => {
@@ -34,40 +35,19 @@ function Allthread() {
     setSortOrder((prevOrder) => (prevOrder === 'ascending' ? 'descending' : 'ascending'))
   }
 
-  const addLike = async (postId) => {
-    const postIndex = posts.findIndex(post => post._id === postId)
-    const post = posts[postIndex]
-    const updatedReactions = [...post.reactions, '65d2f5665305d3958a7ee6e8']
-
-    // update the backend with the updated reactions
-    await fetch(`http://localhost:4001/posts/${postId}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ reactions: updatedReactions }),
-    })
-
-    // fetch the updated post data
-    const response = await fetch(`http://localhost:4001/posts/${postId}`)
-    const updatedPost = await response.json()
-
-    // update the posts state with the updated post
-    const updatedPosts = [...posts]
-    updatedPosts[postIndex] = updatedPost
-    setPosts(updatedPosts)
-  }
 
   return (
     <div>
+      <Filter sortOrder={sortOrder} onToggle={toggleSortOrder} />
       {posts.map((post) => (
         <div key={post._id}>
           {post.isThreadStarter && (
             <>
-              <Filter sortOrder={sortOrder} onToggle={toggleSortOrder} />
               <UserDetails post={post} />
               <DatePost post={post} />
               <TextPost post={post} />
               <ImagePost post={post} />
-              <LikeButton post={post} addLike={addLike} />
+              <LikeButton post={post} setLikes={setLikes} likes={likes} />
               <CommentsCount parentID={post._id} posts={posts}  />
             </>
           )}
