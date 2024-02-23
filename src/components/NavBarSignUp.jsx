bc5e7acf227aab091d808e4cb8735649c2c057cb
alicetra <alicetra0109@gmail.com>
2024-02-23 17:04:35 +1100

    added the link to the register page from the sign up component in the nav bar

--- src/components/NavBarSignUp.jsx
+++ src/components/NavBarSignUp.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './NavBarSignup.css'
+import { Link } from "react-router-dom"
 
 const NavBarSignUp = () => {
 	return (
 		<div className="signup-buttons">
 			<a className="button is-primary signup">
-				<strong>Sign up</strong>
+			<Link to={`/register`}><strong>Sign up</strong></Link>
 			</a>
 			<a className="button is-light login">Log in</a>
 		</div>
