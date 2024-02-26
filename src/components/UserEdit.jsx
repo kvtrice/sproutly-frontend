dac997ff80c88bb5b53892f1751c160d38db7d5f
kvtrice <katricemountford@gmail.com>
2024-02-26 14:32:15 +1100

    updated plant search in edit user profile to beindependent state to the navbar

--- src/components/UserEdit.jsx
+++ src/components/UserEdit.jsx
@@ -7,8 +7,6 @@ import OldPassword from "./user-components/OldPassword.jsx"
 import NavBar from "./NavBar.jsx";
 import './RegisterUser.css'
 
-
-
 const EditUserDetails = ({
 	isDark,
 	setIsDark,
@@ -16,50 +14,44 @@ const EditUserDetails = ({
 	loggedInUserPictureUrl,
 	loggedInUserId,
 }) => {
-	const [username, SetUsername] = useState("")
-	const [password, SetPassword] = useState("")
-	const [oldPassword, SetoldPassword] = useState("")
-	const [selectedPlantTags, setSelectedPlantTags] = useState([])
-	const [imageUrl, setImageUrl] = useState("")
-	const [passwordError, setPasswordError] = useState("")
-	const [oldPasswordError, setoldPasswordError] = useState("")
-	const [usernameError, setUsernameError] = useState("")
+	const [username, SetUsername] = useState("");
+	const [password, SetPassword] = useState("");
+	const [oldPassword, SetoldPassword] = useState("");
+	const [selectedPlantTags, setSelectedPlantTags] = useState([]);
+	const [imageUrl, setImageUrl] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	const [oldPasswordError, setoldPasswordError] = useState("");
+	const [usernameError, setUsernameError] = useState("");
 
-    //hardcoding for testing purpose
-	const user_id = "65d469278aaa81f8f6af8498"
+	useEffect(() => {
+		const fetchPost = async () => {
+			const response = await fetch(
+				`http://localhost:4001/users/${loggedInUserId}`
+			);
+			const data = await response.json();
+			SetUsername(data.username);
+			setSelectedPlantTags(data.plants || []);
+		};
+		fetchPost();
+	}, [loggedInUserId]);
 
-    useEffect(() => {
-        const fetchPost = async () => {
-            const response = await fetch(
-                `http://localhost:4001/users/${user_id}`
-            )
-            const data = await response.json()
-            SetUsername(data.username)
-            setSelectedPlantTags(data.plants|| [])
-        }
-        fetchPost()
-    }, [user_id])
-
-
-
-	async function addUser() {
-        // clearing the error message each time addUser is trigerred so that old error message are not displayed as they are fixed.
-        setUsernameError('')
-        setPasswordError('')
-        setoldPasswordError('')
+	async function editUser() {
+		// clearing the error message each time addUser is trigerred so that old error message are not displayed as they are fixed.
+		setUsernameError("");
+		setPasswordError("");
+		setoldPasswordError("");
 
 		const userDetail = {
 			username: username,
 			newPassword: password,
 			oldPassword: oldPassword,
 			plants: selectedPlantTags,
-			ProfilePicture: imageUrl,
-		}
-
+			profilePicture: imageUrl,
+		};
 
 		try {
 			const putRegister = await fetch(
-				`http://localhost:4001/users/${user_id}`,
+				`http://localhost:4001/users/${loggedInUserId}`,
 				{
 					method: "PUT",
 					headers: {
@@ -67,19 +59,19 @@ const EditUserDetails = ({
 					},
 					body: JSON.stringify(userDetail),
 				}
-			)
+			);
 
 			if (!putRegister.ok) {
-				const errorData = await putRegister.json()
-				console.log("errorData:", errorData)
+				const errorData = await putRegister.json();
+				console.log("errorData:", errorData);
 				errorData.Displayederrors.forEach((error) => {
-					if (error.includes("Incorrect")) setoldPasswordError(error)
-					if (error.includes("minimum")) setPasswordError(error)
-					if (error.includes("Username")) setUsernameError(error)
-				})
+					if (error.includes("Incorrect")) setoldPasswordError(error);
+					if (error.includes("minimum")) setPasswordError(error);
+					if (error.includes("Username")) setUsernameError(error);
+				});
 			}
 		} catch (err) {
-			console.error(err.message)
+			console.error(err.message);
 		}
 	}
 
@@ -88,8 +80,6 @@ const EditUserDetails = ({
 			<NavBar
 				isDark={isDark}
 				setIsDark={setIsDark}
-				selectedPlantTags={selectedPlantTags}
-				setSelectedPlantTags={setSelectedPlantTags}
 				loggedInUserPictureUrl={loggedInUserPictureUrl}
 				isUserLoggedIn={isUserLoggedIn}
 				loggedInUserId={loggedInUserId}
@@ -105,7 +95,7 @@ const EditUserDetails = ({
 						</label>
 						<PostUsername
 							SetUsername={SetUsername}
-							username={username}
+							username={username || ""}
 						/>
 						{usernameError && (
 							<p className="has-text-danger">{usernameError}</p>
@@ -140,8 +130,6 @@ const EditUserDetails = ({
 								</p>
 							)}
 						</div>
-
-                        
 					</div>
 					<div className="field profile-picture-upload">
 						<label className="text" htmlFor="profilePicture">
@@ -154,13 +142,13 @@ const EditUserDetails = ({
 					</div>
 					<label className="text">Update the plants you own:</label>
 					<PlantSearch
-					setSelectedPlantTags={setSelectedPlantTags}
-                    selectedPlantTags={selectedPlantTags}
+						setSelectedPlantTags={setSelectedPlantTags}
+						selectedPlantTags={selectedPlantTags}
 					/>
 					<div className="save-button">
 						<button
 							className="is-primary button"
-							onClick={() => addUser(username)}
+							onClick={() => editUser(username)}
 						>
 							Save
 						</button>
